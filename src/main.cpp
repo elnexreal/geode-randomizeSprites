5863508db302a956e724a4d6bdfc735ca3eca833
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-02-05 15:09:50 -0300

    basic logic thingy, not finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CCLayer.hpp>
 
 using namespace geode::prelude;
 
-struct randomizeSprites : Modify<randomizeSprites, MenuLayer> {
-    void onMoreGames() {
-        log::info("Hello Geode!");
+struct randomizeSprites : Modify<randomizeSprites, CCLayer> {
+    void onEnterTransitionDidFinish() {
+        CCLayer::onEnterTransitionDidFinish();
+
+        CCArray *childArray = this->getChildren();
+
+        log::debug("CClayer created");
+
+        for (auto *i : CCArrayExt<CCObject *>(childArray)) {
+            if (auto spr = typeinfo_cast<CCSprite *>(i)) {
+                log::info("CCSprite {}", spr);
+            } else if (auto btn = typeinfo_cast<CCMenuItemSpriteExtra *>(i)) {
+                log::info("MenuItemSprite {}", btn);
+            }
+        }
     }
 };
